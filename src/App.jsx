1f6bd463c1a593a190f64c68d2269481c28d54b5
alexanderassuresoft <alexis.0804yes@gmail.com>
2023-11-11 21:36:59 -0400

    style: add hero section and card description

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
-import ProfileCard from './ProfileCard'
-import AlexaImage from './assets/alexa.png'
-import CortanaImage from './assets/cortana.png'
-import SiriImage from './assets/siri.png'
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./assets/alexa.png";
+import CortanaImage from "./assets/cortana.png";
+import SiriImage from "./assets/siri.png";
 function App() {
   return (
     <>
-    
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital assistants</p>
+        </div>
+      </section>
 
-    <ProfileCard title= "Alexa" handle = "@alexa99" image={AlexaImage} />
-    <ProfileCard title= "Ana" handle = "@ana99"image={CortanaImage}/>
-    <ProfileCard title= "Juan" handle = "@juan99" image={SiriImage}/>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="In nisi qui ex consequat amet minim veniam laboris irure."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Ana"
+                handle="@ana99"
+                image={CortanaImage}
+                description="In nisi qui ex consequat amet minim veniam laboris irure."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Juan"
+                handle="@juan99"
+                image={SiriImage}
+                description="In nisi qui ex consequat amet minim veniam laboris irure."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-    
-      
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,19 +1,30 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function ProfileCard({title, handle,image}) {
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <>
-        <div>Title: {title}</div>
-        <div>Handle: {handle}</div>
-        <img src={image}></img>
-    </>
-  )
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image}></img>
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title: {title}</p>
+          <p className="subtitle is-6">Handle: {handle}</p>
+          <p className="subtitle is-6">Description: {description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ProfileCard.propTypes = {
   title: PropTypes.string.isRequired,
   handle: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
 };
 
 export default ProfileCard;
